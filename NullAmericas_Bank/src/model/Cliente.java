2e17f28a29f08927511adcdadd6ba029f39b175d
vhleitee <vitorhugoleite375@gmail.com>
2026-06-13 08:35:48 -0300

    Updade class Cliete.java

--- NullAmericas_Bank/src/model/Cliente.java
+++ NullAmericas_Bank/src/model/Cliente.java
@@ -1,26 +1,9 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class Cliente {
-	List<Conta> contas = new ArrayList<>();
-	
 
 	public Cliente() {
 		
-	}
-	
-	public Cliente(List<Conta> contas) {
-		this.setContas(contas);
-	}
-
-	public List<Conta> getContas() {
-		return contas;
-	}
-
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
 	}
 	
 }
